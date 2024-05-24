86b87262feb33263a622ad5702bc2bca6ec682bc
Sarah Kamala <sarahkamala868@gmail.com>
2024-05-24 19:13:06 +0200

    Classe Pediatre

--- examen.py
+++ examen.py
@@ -33,3 +33,27 @@ class Medecin(Employe):
     """ Polymorphisme """
     def action(self):
         print("Le médecin examine le patient.")
+
+
+    """ Heritage de la classe, l'encapsulation et le polymorphisme """
+class Pediatre(Employe):
+    def __init__(self, nom, specialite, service):
+        self.__nom = nom
+        self.__specialite = specialite
+        self.service = service
+    """ Le get et  le set pour acceder aux methodes prives """
+    def getnom(self):
+        return self.__nom
+    def setnom(self,nom):
+        self.__nom = nom
+    def getspecialite(self):
+        return self.__specialite
+    def setspecialite(self,specialite):
+        self.__specialite=specialite
+    """ Creation des methodes """
+    # Surcharge de la méthode afficher_infos pour inclure le service
+    def afficher_infos(self):
+        print(f"Nom: {self.__nom}\nSpecialite: {self.__specialite}\nService: {self.service}")
+    """ Polymorphisme """
+    def action(self):
+        print("Le pédiatre prescrit un vaccin.")
